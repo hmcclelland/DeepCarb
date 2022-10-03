38ae49aafae002d304e79ee3f4670aab86c955df
David Evans <47573856+dbjevans@users.noreply.github.com>
2022-10-03 19:22:11 +0200

    Add files via upload

--- deepCarb_v0_147P.m
+++ deepCarb_v0_147P.m
@@ -1,7 +1,7 @@
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%%%%%%%%%%%%%%%%%%% This is DeepCarb version 0.147 %%%%%%%%%%%%%%%%%%%%%%%
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%
+% %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% %%%%%%%%%%%%%%%%%% This is DeepCarb version 0.147 %%%%%%%%%%%%%%%%%%%%%%%
+% %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% 
 % DeepCarb is a simple carbon cycle box model consisting of three boxes
 % (atmosphere, surface ocean, deep ocean) and two infinite reservoirs
 % (terrestrial silicate rock and deep ocean CaCO3). It is designed to
@@ -9,7 +9,7 @@
 % seawater carbonate chemistry, particularly over i) long timescales and
 % ii) in a Monte Carlo fashion with random permutations of key model
 % parameterisations.
-%
+% 
 % By default, the model can be run in one of two ways. Setting the variable
 % nMC = 1 will result in a simulation run only once using the default set
 % of model parameterisations. Setting nMC = n will result in n model
@@ -20,7 +20,7 @@
 % saturation state of the deep ocean box, and iii) the slope of the
 % relationship between CaCO3 production in the surface ocean and the
 % saturation state of the surface ocean box.
-%
+% 
 % To maintain flexibility, this script is not a function. Before running
 % the model, at least four parameters need to be manually changed in the
 % script. These are:
@@ -40,11 +40,11 @@
 %      rates could be modified (w12), seawater [Ca] could be changed either 
 %      directly (oceanCa), or via the Ca/total cation ratio of the
 %      weathering product (weatherCa).
-%
+% 
 % In addition, the script produces a plot of the results. Edit linlog to 
 % control whether the data are shown on a linear or logarithmic scale 
 % (1 = linear, 0  = log).
-%
+% 
 % The results are contained in the cell array dataOut which has dimensions 
 % n*1. The format of each cell is an ageEnd*15 matrix, with the following 
 % columns:
@@ -65,8 +65,8 @@
 %       Negative values represent net burial
 %   14) Carbon drawdown as a result of silicate weathering (Gt/year)
 %   15) Change in CaCO3 export from the surface ocean box (moles/year)
-%
-%
+% 
+% 
 % This script depends on the following functions which are packaged here
 % but are not my work. Please give appropriate credit to the original
 % authors.
@@ -80,28 +80,28 @@
 % - The parallel computing toolbox is required if parfor is used.
 % - The random number generator (normrnd) requires the statistics and
 % machine learning toolbox
-%
-%
+% 
+% 
 % ------------------------------- EXAMPLES -------------------------------
-%
+% 
 % Where these example recommend changing the values of nMC, linlog, ageEnd,
 % and the use or otherwise of the parallel computing toolbox, these can be
 % found on lines 135, 140, 136, and 156 respectively. In the latter case,
 % 'for' simply needs to be replaced with 'parfor'.
-%
+% 
 % 1. Simulate the anthropogenic C release and run the model once using the
 % default settings. Set nMC = 1, linlog = 0 (to view the long and short
 % term effects, ageEnd = 1e6, and use a for loop rather than parfor.
 % Uncomment lines 274-275 to load the anthropogenic C release data, and
 % comment line 280 specifying no atmospheric CO2 perturbation.
-%
+% 
 % 2. Simulate the PETM and run the model with 100 random permutations of
 % the key model parameterisations. Set nMC = 100, linlog = 1, ageEnd =
 % 2.5e5, and use the parallel computing toolbox (change the for loop on
 % line 156 to a parfor loop). Uncomment line 263 to perturb the model with
 % one possible PETM C-release scenario, and comment line 280 specifying no
 % atmospheric CO2 perturbation.
-%
+% 
 % 3. Prescribe an atmospheric CO2 doubling over a range of doubling rates.
 % Set nMC = 50, linlog = 0, ageEnd = 1e7, and use the parallel computing
 % toolbox. Uncomment the code on lines 339-347, which will vary the
@@ -109,7 +109,7 @@
 % in random selections of the key model parameterisations described above.
 % To maintain consistency between the doubling rate experiments, remove
 % this feature by uncommenting line 235.
-%
+% 
 % 4. Decrease seawater [Ca] starting from Eocene boundary conditions and
 % run a full Monte Carlo simulation. Set nMC = 1000, linlog = 1, ageEnd =
 % 15e6, and use the parallel computing toolbox. Uncomment line 293 to drive
@@ -117,27 +117,27 @@
 % let the model equilibrate. Comment out line 294 which specifies no
 % seawater [Ca] perturbation. Comment out lines 162-163 which load the 
 % modern stable conditions, and uncomment lines 165-166 to load Eocene
-% boundary conditions.
-%
+% boundary conditions. (note, this will take ~2 hours to run)
+% 
 % 5. Drive CO2 down by increasing the weathering rate independently of
 % climate, and run a full Monte Carlo simulation. Set nMC = 1000, linlog =
 % 1, ageEnd = 15e6, and use the parallel computing toolbox. Uncomment lines
 % 383-389 to increase the silicate weathering rate by 5% over 4 Myr,
 % starting at model year 1 million, and then hold it at that point. Comment
 % out lines 162-163 which load the modern stable conditions, and uncomment
-% lines 165-166 to load Eocene boundary conditions.
-%
+% lines 165-166 to load Eocene boundary conditions. (note, this will take 
+% ~2 hours to run)
+% 
 % ------------------------------------------------------------------------
-
 clear vars
 
 % edit key model setup parameters
 nMC = 1;   % number of MC simulations. change for to parfor if >~10!
-ageEnd = 2.5e5;  % how many model years to run?
+ageEnd = 1e6;  % how many model years to run?
 
 % edit figure properties
 figN = 1; % figure number
-linlog = 1; % edit here, log = 0
+linlog = 0; % edit here, log = 0
 if nMC<11    % set  plot transparency
     trans = 1;
 else
